96ac2527cadc6ea004a43edac27902828608037f
Lucas Debouté <lucas.deboute@gmail.com>
2020-06-14 16:28:08 +0200

    feat(login): protected routes with authentication

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Switch, Route, Redirect} from 'react-router-dom'
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Login from './pages/login';
+
+interface IAuthentificatedRoute { 
+  component: any;
+  auth: boolean;
+  path: string;
+  exact: boolean;
+}
+
+function AuthenticatedRoute(params: IAuthentificatedRoute) {
+  return (
+    <Route  
+      render={(props) => params.auth === true
+      ? <params.component params />
+      : <Redirect to={{pathname: '/login'}} />}
+    />
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <AuthenticatedRoute auth={false} exact path="/" component={App} />
+    </Switch>
   </React.StrictMode>,
   document.getElementById('root')
 );
